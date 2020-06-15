3161f3d880204c7279fa5d075272a52cc445845e
Weilbyte <git@weilbyte.dev>
2020-06-15 13:46:17 +0200

    pyflakes shut the fuck up

--- pfp_lgbt/__init__.py
+++ pfp_lgbt/__init__.py
@@ -3,6 +3,6 @@ __author__ = 'Weilbyte'
 __license__ = 'MIT'
 __version__ = '0.0.1'
 
-from .client import Client # noqa
-from .models import Flag # noqa
-from .error import UnsupportedMIMEError, ConvertImageError # noqa
+from .client import Client  # noqa: F401
+from .models import Flag  # noqa: F401
+from .error import UnsupportedMIMEError, ConvertImageError  # noqa: F401
