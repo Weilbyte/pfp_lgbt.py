6e2059bbdcc2e531a8d2e1df81bb86a69d626341
Weilbyte <git@weilbyte.dev>
2020-06-16 09:46:49 +0200

    Rename back to underscore

--- pfp_lgbt/__init__.py
+++ pfp_lgbt/__init__.py
@@ -1,7 +1,7 @@
-__title__ = 'pfp-lgbt'
+__title__ = 'pfp_lgbt'
 __author__ = 'Weilbyte'
 __license__ = 'MIT'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from .client import Client  # noqa: F401
 from .models import Flag  # noqa: F401
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pfp-lgbt',
-    version='1.0.0',
+    name='pfp_lgbt',
+    version='1.0.1',
     author='Weilbyte',
     description='Python API Wrapper for https://pfp.lgbt/',
     long_description=long_description,
